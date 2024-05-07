52b1be69b0b288d89729210f70177f92eb78638b
SelmaFanani <selma.fanani@etu.univ-amu.fr>
2024-05-07 14:30:46 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,4 +5,13 @@ Created on Tue May  7 14:23:05 2024
 
 @author: selmafanani
 """
+import os
+import Denoise.python3
+from multiprocessing import Pool
 
+
+
+if __name__ == '__main__':
+  #multiprocessing allows us to parallely finish off all images!
+	pool = Pool(processes=os.cpu_count())
+	pool.map(Denoise.denoise, [1, 2, 3, 4, 14, 15, 16, 18])
