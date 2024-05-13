6692910c425d011c033e2c0849daa04287ea686a
alexxxx1383 <169162241+alexxxx1383@users.noreply.github.com>
2024-05-13 17:06:17 +0200

    Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -7,6 +7,7 @@ Created on Tue May  7 14:23:05 2024
 """
 import os
 import Denoise.python3
+from Tests import Tests
 from multiprocessing import Pool
 
 
@@ -15,3 +16,9 @@ if __name__ == '__main__':
   #multiprocessing allows us to parallely finish off all images!
 	pool = Pool(processes=os.cpu_count())
 	pool.map(Denoise.denoise, [1, 2, 3, 4, 14, 15, 16, 18])
+
+# Tests
+image_path1 = 'image1.png'
+image_path2 = 'image2.png'
+print(compare_images(image_path1, image_path2))
+
